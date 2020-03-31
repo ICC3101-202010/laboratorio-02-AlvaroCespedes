5c6982a90add73ae5df1f1ba1672a446bf94a799
AlvaroCespedes <aecespedes@miuandes.cl>
2020-03-31 08:48:07 -0300

    ready Cancion class

--- Lab 2/Cancion.cs
+++ Lab 2/Cancion.cs
@@ -4,39 +4,25 @@ using System.Text;
 
 namespace Lab_2
 {
-    public class Cancion
+    class Cancion
     {
         public string Nombre;
         public string Album;
         public string Artista;
         public string Genero;
-        List<string> ListaCanciones; //Creo la lista de la clase cancion
-        public Cancion()//Creo el constructor de la lista 
-        {
-            ListaCanciones = new List<string>();
 
-        }
         public string Informacion() // Primer Metodo de cancion
         {
-            Console.WriteLine("Informacion de la cancion");
-            {
-                return null;
-            }
+            return ($"genero: {Genero}, artista: {Artista}, album: {Album}, nombre: {Nombre}");
         }
-        public Cancion (string nombre, string album, string artista, string genero) // Creo el constructor de la clase cancion
+
+        public Cancion(string nombre, string album, string artista, string genero) // Creo el constructor de la clase cancion
         {
             this.Nombre = nombre;
             this.Album = album;
             this.Artista = artista;
             this.Genero = genero;
         }
-    }
 
-    public class Espotifai //Solo tiene metodos relacionados con Cancion
-    {
-        public void AgregarCancion(string cancion)
-        {
-            lista1.Add(cancion);
-        }
     }
 }
